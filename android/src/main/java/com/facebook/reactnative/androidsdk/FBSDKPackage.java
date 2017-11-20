57faacdacda61b7e8317764928a5a4bcbd4681a3
muhammad umair javaid <muhammadumairjavaid@gmail.com>
2017-11-20 16:46:34 +0500

    Override error solved

--- android/src/main/java/com/facebook/reactnative/androidsdk/FBSDKPackage.java
+++ android/src/main/java/com/facebook/reactnative/androidsdk/FBSDKPackage.java
@@ -58,7 +58,6 @@ public class FBSDKPackage implements ReactPackage {
         );
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList(
                 new FBLikeViewManager(),
